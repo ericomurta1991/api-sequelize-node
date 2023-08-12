68279a982d41772dac41ff5f784947b98d4937c8
ericomurta1991 <erico_murta@yahoo.com.br>
2023-08-12 16:15:01 -0300

    setadas todas as rotas dos controladores da api

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,8 +1,12 @@
-const bodyParser = require('body-parser');
+const bodyParser = require('body-parser')
 const pessoas = require('./pessoasroutes')
+const niveis = require('./niveisroutes')
+const turmas = require('./turmasroutes')
 
  module.exports = app => {
     app.use(bodyParser.json())
     app.use(pessoas)
+    app.use(niveis)
+    app.use(turmas)
 
  }
