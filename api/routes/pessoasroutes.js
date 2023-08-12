a48649d3e925ade04bef649e7a5eeb537074c93a
ericomurta1991 <erico_murta@yahoo.com.br>
2023-08-12 16:10:46 -0300

    adicionado novas rotas para os metodos de matriculas

--- api/routes/pessoasroutes.js
+++ api/routes/pessoasroutes.js
@@ -6,8 +6,13 @@ const router = Router()
 
 router.get('/pessoas', PessoaController.pegartTodasAsPessoas)
 router.get('/pessoas/:id', PessoaController.pegaUmaPessoa)
+router.get('/pessoas/:estudanteId/matricula/:matriculaId', PessoaController.pegaUmaMatricula)
 router.post('/pessoas', PessoaController.criarPessoa)
+router.post('/pessoas/:estudanteId/matricula', PessoaController.criarMatricula)
 router.put('/pessoas/:id', PessoaController.atualizaPessoa)
+router.put('/pessoas/:estudanteId/matricula/:matriculaId', PessoaController.atualizaMatricula)
 router.delete('/pessoas/:id', PessoaController.apagarPessoa)
+router.delete('/pessoas/:estudanteId/matricula/:matriculaId', PessoaController.apagaMatricula)
+
 
 module.exports = router;
